e5e3a73d3f7974906cdb00ec1afd84120027c982
willearysonborges <wborgesnunes@gmail.com>
2024-07-06 13:48:41 -0300

    segundo commit

--- gt-todo-list/src/App.jsx
+++ gt-todo-list/src/App.jsx
@@ -1,33 +1,71 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [inputValue, setInputValue] = useState("")
+  const [textareaValue, setTextareaValue] = useState("")
+
+  const [todos, setTodos] = useState([
+   
+  ])
+  function handleAddNewTodo(event){
+    event.preventDefault()
+    setTodos((state) =>{
+      const newTodo ={
+        title:inputValue,
+        description:textareaValue
+      }
+     return[...todos, newTodo]
+    })
+
+    setInputValue('')
+    setTextareaValue('')
+  }
+
+  function removetodo(id){
+    const newTodoList = todos.filter((todo) => todo.id !== id)
+    setTodos(newTodoList)
+  }
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header>
+        <h1>ToDo List</h1>
+      </header>
+      <main>
+          <div className='form-area'>
+            <form onSubmit={handleAddNewTodo} action="">
+              <label htmlFor="">Tarefa</label>
+              <input type="text" 
+              className='todo-input' 
+              value={inputValue}
+              onChange={(event) => setInputValue(event.target.value)}
+              />
+              <label htmlFor="">Descrição da tarefa</label>
+              <textarea name="" id="" 
+               value={textareaValue}
+               onChange={(event) => setTextareaValue(event.target.value)}
+              ></textarea>
+              <button type='submmit'> Adicionar</button>
+            </form>
+            <div className='list-item-area'>
+              <ul className='list-todo'>
+                {todos?.map((todo, index) =>(
+                  <li key={index}>
+                    <input type="checkbox" onClick={() => removetodo(todo.id)}/>
+                    <label>{todo.title}</label>
+                    <p>{todo.description}</p>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+          
+      </main>
+      <footer>
+        <p></p>
+      </footer>
     </>
   )
 }
